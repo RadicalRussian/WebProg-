bf85f8f77f36a98a7b8916c8c4a614a33f042a78
RadicalRussian <markuskostrizki@web.de>
2022-06-18 11:29:54 +0200

    First Commit

---
+++ setup.php
@@ -0,0 +1,17 @@
+<?php
+
+function setupDB() {
+  $pdo = new PDO("sqlite:C:\\xampp\\htdocs\\toymodels\\webaw_exercises\\toymodels\\toymodels.db");
+  
+  $sql = "CREATE TABLE users (id INTEGER PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), salt VARCHAR(255));";
+
+  $result = $pdo->exec($sql);
+
+  if ($result === false) {
+    die("Oops an error occured");
+  } else {
+    echo "Table created successfully!";
+  }
+}
+
+setupDB();
